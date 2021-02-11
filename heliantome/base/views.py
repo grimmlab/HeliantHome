666cbdd607b07bcb980a54cef251ad404e0c7fd7
dominikgrimm <dominik.grimm@tum.de>
2021-02-11 20:47:02 +0100

    initial landing page lanched

--- heliantome/base/views.py
+++ heliantome/base/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+import main.models as models
+
 '''
 Landing Page
 '''
 def landing_page(request):
-    return render(request,'base/landingpage.html',{})
+    vdata = {}
+    vdata['number_species'] = models.Species.objects.count()
+    vdata['number_phenotypes'] = models.Phenotype.objects.count()
+    vdata['number_populations'] = models.Population.objects.count()
+    vdata['number_individuals'] = models.Individual.objects.count()
+    vdata['number_images'] = models.PlantImage.objects.count()
+    return render(request,'base/landingpage.html',vdata)
