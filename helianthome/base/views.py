5911504e5d11c17781661d958a6e02e2bb9b1c2d
root <root@tucs000-helianthome.srv.mwn.de>
2021-06-13 18:20:08 +0200

    modified landing page

--- helianthome/base/views.py
+++ helianthome/base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 import json
+import numpy as np
 
 import main.models as models
 
@@ -31,7 +32,14 @@ def landing_page(request):
     vdata['number_populations'] = models.Population.objects.count()
     vdata['number_individuals'] = models.Individual.objects.count()
     vdata['number_images'] = models.PlantImage.objects.count()
-    
+    categories = models.Phenotype.objects.values_list("category").distinct()
+    category_list = []
+    category_entries = []
+    for cat in categories:
+        category_list.append(str(cat[0]))
+        category_entries.append(models.Phenotype.objects.filter(category=cat[0]).count())
+    vdata['category_list'] = json.dumps(category_list)
+    vdata['category_entries'] = json.dumps(list(np.array(category_entries,dtype="float")))
     pops = models.Population.objects.all()
     data = [{"latLng": [pop.latitude, pop.longitude], "name": pop.species.species + ": " + pop.population_id + " (" + pop.country + ", " + pop.sitename + ")", "style": {"fill": marker_color(pop.species.species),"r":4,"opacity":0.6}} for pop in pops]   
     vdata['map_data'] = json.dumps(data)
